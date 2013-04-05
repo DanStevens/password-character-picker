2d21847990099b7995ecbe447f13112949c9bb7a
Daniel Stevens <dan.stevens@oracle.com>
2013-04-05 10:54:44 +0100

    Basic functionality complete.

--- data/panel.js
+++ data/panel.js
@@ -1,17 +1,30 @@
+// Config
+var cellSize = 15; // pixels
+
 $(document).ready(function() {
-  $('phraseField').on('keyup', phraseFieldKeyUpHander);
-  phraseFieldKeyUpHander();
+  $('#indexTable').hide();
+  $('#phraseField').on('keyup', phraseFieldKeyUpHander);
+  $('#positionRow').height(cellSize);
+  $('#characterRow').height(cellSize);
 });
 
 function phraseFieldKeyUpHander()
 {
-    $('indexTable').hide();
-    if ($('phraseField').val() !== "") {
-        generateIndexTable(phraseField.val());
-        $('indexTable').show();
-    }
+  $('#indexTable').hide();
+  if ($('#phraseField').val() !== "") {
+    generateIndexTable($('#phraseField').val());
+    $('#indexTable').show();
+  }
 }
 
 function generateIndexTable(phrase) {
-    console.log("Generating table using '" + phrase + "'");
+  $('#positionRow').empty();
+  $('#characterRow').empty(); 
+  
+  for (var i = 0; i < phrase.length; i++) {
+    $('<td>' + (i + 1).toString() + '</td>').width(cellSize).appendTo('#positionRow');
+    $('<td>' + phrase[i] + '</td>').width(cellSize).appendTo('#characterRow');
+  }
+  
+  $('#indexTable').width(cellSize * phrase.length);
 }
