ea7dc5afa1e87fd778b6a99e5d8e746d513e5c91
Dan Stevens <dan.stevens@oracle.com>
2016-02-25 17:47:43 +0000

    Improvements to Javascript syntax standard compliance.

--- src/lib/main.js
+++ src/lib/main.js
@@ -1,38 +1,53 @@
-var widgets = require("sdk/widget");
+"use strict";
+var ui = require("sdk/ui");
 var data = require("sdk/self").data;
 var panels = require("sdk/panel");
 var clipboard = require("sdk/clipboard");
 var prefs = require("sdk/simple-prefs");
 var tabs = require("sdk/tabs");
+var panel, widget;
 
-var panel = panels.Panel({
-  width: 500,
-  height: 175,
-  contentURL: data.url("panel/page.html")
+panel = panels.Panel({
+    width: 500,
+    height: 175,
+    contentURL: data.url("panel/page.html"),
+    onHide: function () {
+        widget.state('window', {checked: false});
+    }
 });
 
-var widget = widgets.Widget({
-  id: "widget",
-  label: "Password Character Picker",
-  contentURL: data.url("widget.png"),
-  panel: panel
+widget = ui.ToggleButton({
+    id: "widget",
+    label: "Password Character Picker",
+    icon: {
+        "16": "./widget16.png",
+        "32": "./widget32.png",
+        "48": "./widget48.png",
+        "64": "./widget64.png"
+    },
+    onChange: function (state) {
+        if (state.checked) {
+            panel.show({position: widget});
+        }
+    }
 });
 
 function init() {
-  panel.on("show", function () {
-    panel.port.emit("show", prefs);
-  });
+    panel.on("show", function () {
+        panel.port.emit("show", prefs);
+    });
 
-  panel.port.on("setClipboard", function (value) {
-    clipboard.set(value, "text");
-    console.log("Clipboard set to: " + value);
-    panel.hide();
-  });
+    panel.port.on("setClipboard", function (value) {
+        clipboard.set(value, "text");
+        console.log("Clipboard set to: " + value);
+        panel.hide();
+    });
 
-  tabs.on("ready", function (tab) {
-    if (prefs.prefs["clearPhraseOnPageTurn"] && tab == tabs.activeTab)
-      panel.port.emit("clearPhraseInput");
-  });
+    tabs.on("ready", function (tab) {
+        if (prefs.prefs.clearPhraseOnPageTurn && tab === tabs.activeTab) {
+            panel.port.emit("clearPhraseInput");
+        }
+    });
 }
 
 init();
