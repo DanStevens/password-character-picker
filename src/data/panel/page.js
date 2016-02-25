66b443ca27692c22344859ad61e04de4526cb1a1
Dan Stevens <dan.stevens@oracle.com>
2016-02-25 17:47:05 +0000

    Removed dependency on JQuery.

--- src/data/panel/page.js
+++ src/data/panel/page.js
@@ -1,50 +1,63 @@
 // Config
-var cellSize = 24; // pixels
-var maskChar = '•';
+var cellSize = "24px";
+var maskChar = 'â€¢';
 var maskButtons = false;
 
+var positionRow = document.getElementById('positionRow');
+var characterRow = document.getElementById('characterRow');
+var phraseField = document.getElementById('phraseField');
+var indexTable = document.getElementById('indexTable');
+var indexTableContainer = document.getElementById('indexTableContainer');
+
 addon.port.on("show", function (args) {
-  maskButtons = args.prefs["maskButtons"];
-  $('#positionRow').height(cellSize);
-  $('#characterRow').height(cellSize);
-  $('#phraseField').attr("type", args.prefs["maskPhraseInput"] ? "password" : "text");
-  $('#phraseField').on('keyup', phraseFieldKeyUpHander);
-  phraseFieldKeyUpHander();
+    maskButtons = args.prefs.maskButtons;
+    positionRow.style.height = characterRow.style.height = cellSize;
+    phraseField.setAttribute("type", args.prefs.maskPhraseInput ? "password" : "text");
+    phraseField.addEventListener('keyup', phraseFieldKeyUpHander);
+    phraseFieldKeyUpHander();
 });
 
 addon.port.on("clearPhraseInput", function (args) {
-  $('#phraseField').val("");
+    phraseField.value = '';
 });
 
-function phraseFieldKeyUpHander()
-{
-  $('#indexTableContainer').hide();
-  if ($('#phraseField').val() !== "") {
-    generateindexTableContainer($('#phraseField').val());
-    $('#indexTableContainer').show();
-  }
+function phraseFieldKeyUpHander() {
+    indexTableContainer.style.display = 'none';
+    if (phraseField.value !== "") {
+        generateindexTableContainer(phraseField.value);
+        indexTableContainer.style.display = '';
+    }
 }
 
 function generateindexTableContainer(phrase) {
-  $('#positionRow').empty();
-  $('#characterRow').empty();
-  
-  for (var i = 0; i < phrase.length; i++) {
-    $('<td>' + (i + 1).toString() + '</td>').width(cellSize).appendTo('#positionRow');
-    var charTd = $('<td>').width(cellSize);
-    $('#characterRow').append(charTd);
-    if (maskButtons || phrase[i] !== ' ') {
-      var charButton = $('<button>')
-        .val(phrase[i])
-        .text(maskButtons ? maskChar : phrase[i])
-        .click(characterButtonClickHandler);
-      charTd.append(charButton);
-    }
-  }
-  
-  $('#indexTable').width(cellSize * phrase.length);
+    positionRow.innerHTML = characterRow.innerHTML = '';
+    
+    phrase.split('').forEach(function (character, i) {
+        var positionCell = createTableCell((i + 1).toString(), cellSize);
+        positionRow.appendChild(positionCell);
+        
+        var characterCell = createTableCell("", cellSize);
+        characterRow.appendChild(characterCell);
+        
+        if (maskButtons || character !== ' ') {
+            var characterButton = document.createElement('button');
+            characterButton.value = character;
+            characterButton.innerHTML = maskButtons ? maskChar : character;
+            characterButton.addEventListener('click', characterButtonClickHandler);
+            characterCell.appendChild(characterButton);
+        }
+    });
+
+    indexTable.style.width = cellSize * phrase.length;
+}
+
+function createTableCell(contents, width) {
+    var cell = document.createElement('td');
+    cell.innerHTML = contents;
+    cell.style.width = width;
+    return cell;
 }
 
 function characterButtonClickHandler() {
-  addon.port.emit("setClipboard", $(this).val());
+    addon.port.emit("setClipboard", this.value);
 }
